f387d0db42cc3cdc3876068ef174bd13ed95fe97
Wooyeong Kim <kjw8922@naver.com>
2022-12-09 17:46:48 +0700

    first commit

---
+++ pratice.py
@@ -0,0 +1,53 @@
+# input : 사용자 입력을 받는 함수
+# language = input("무슨 언어를 좋아하세요?")
+# print("{}은 아주 좋은 언어입니다.!".format(language))
+
+# dir : 어떤 객체를 넘겨줬을때 그 객체가 어떤 변수와 함수를 가지고 있는지 표시
+# print(dir())
+# import random
+# print(dir())
+# import pickle
+# print(dir())
+
+# print(dir(random))
+
+# lst = [1, 2, 3]
+# print(dir(lst))
+
+# name = "Jin"
+# print(dir(name))
+
+
+# glob : 경로 내의 폴더 / 파일 목록 조회 (윈도우 dir)
+# import glob
+# print(glob.glob("*.py")) # 확장자가 py 인 모든 파일
+
+# import os
+# print(os.getcwd()) # 현재 디렉토리
+
+# folder = "sample_dir"
+# if os.path.exists(folder):
+#     print("이미 존재하는 폴더입니다.")
+#     os.rmdir(folder)
+#     print(folder, "폴더를 삭제하였습니다.")
+# else:
+#     os.makedirs(folder) # 폴더 샹성
+#     print(folder, "폴더를 생성하였습니다.")
+
+# print(os.listdir())
+
+# time : 시간 관련 함수
+# import time
+# print(time.localtime())
+# print(time.strftime("%Y-%m-%d %H:%M:%S"))
+
+# import datetime
+# print("오늘 날짜는 ", datetime.date.today())
+
+# timedelta : 두 날짜 사이의 간격
+# today = datetime.date.today()
+# td = datetime.timedelta(days=100)
+# print("우리가 만난지 100일은", today + td)
+
+import byme
+byme.sign()
